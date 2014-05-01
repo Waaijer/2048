18f6fab8ffa837c6168f7fd52b15d3a4f9af928e
Marlen Waaijer <marlen.waaijer@gmail.com>
2014-05-01 19:04:13 -0400

    Small changes to colors.
    Related to opacity, used rgba instead.
    Added smaller background image for the smaller screen.
    Fixed the color scheme for the ipod

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -52,18 +52,18 @@ HTMLActuator.prototype.clearContainer = function (container) {
 
 HTMLActuator.prototype.addTile = function (tile) {
   var valueMap = {
-    2 :    '<Udacity>',
-    4 :    '<Intro CS>',
-    8 :    "skills=['code']",
-    16 :   "skills.add('CSS')",
-    32 :   '</Intro CS>',
-    64 :   '<Job search>',
-    128 :  'getJob(skills)',
-    256 :  '</Job search>',
-    512 :  'if Udacious:',
-    1024 : 'skills.increase()',
-    2048 : 'myJob.advance()',
-    4096 : 'myCareer=myJob'
+    2 :    '<Neptune>',
+    4 :    '<Uranus>',
+    8 :    'Saturn',
+    16 :   'Io',
+    32 :   'Jupiter',
+    64 :   'Mars',
+    128 :  'Moon',
+    256 :  'Earth',
+    512 :  'Venus',
+    1024 : 'Mercury',
+    2048 : 'Sun',
+    4096 : 'Milky Way'
   }
   var self = this;
 
